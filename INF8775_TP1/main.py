c06f85cc2363ad20909c20167515f37105f6f51a
polarbuzzle <fersamw@gmail.com>
2020-09-23 14:49:50 -0400

    Closest point execution in function

--- INF8775_TP1/main.py
+++ INF8775_TP1/main.py
@@ -1,33 +1,13 @@
 import sys
 
-from INF8775_TP1.closest_points.brute_force import execute_brute_force
-from INF8775_TP1.closest_points.DpR import execute_DpR
+from closest_points.brute_force import execute_brute_force
+from closest_points.DpR import execute_DpR
 from copy import deepcopy
 
 NB_REP = 10
 
-if __name__ == "__main__":
 
-    file_name = ''
-
-    if (len(sys.argv) <= 1):
-        exit('Erreur: Pas assez d\'arguments. Vous devez indiquer le \
-    nombre de points à générer et le nom du fichier de sortie.')
-
-    if (len(sys.argv) > 2):
-        exit('Erreur: Trop d\'arguments.')
-
-    try:
-        file_name = str(sys.argv[1])
-
-        if file_name == "":
-            raise ValueError()
-    except:
-        exit('Erreur: Le premier argument (nombre de points) doit être \
-    un entier positif.')
-
-    print(file_name)
-
+def execute_closest_points(file_name):
     n = 0
     points = []
 
@@ -46,8 +26,30 @@ if __name__ == "__main__":
 
     for i in range(NB_REP):
         time = execute_DpR(points_x, points_y, 5)
-        print("Run #" + str(i + 1) + ": " + str(time) + "s")
+        # print("Run #" + str(i + 1) + ": " + str(time) + "s")
         average_time += time
 
     average_time /= NB_REP
     print("Average time: " + str(average_time) + "s")
+
+
+if __name__ == "__main__":
+
+    file_name = ""
+
+    if (len(sys.argv) <= 1):
+        exit('Erreur: Pas assez d\'arguments. Vous devez indiquer le \
+              nombre de points à générer et le nom du fichier de sortie.')
+
+    if (len(sys.argv) > 2):
+        exit('Erreur: Trop d\'arguments.')
+
+    try:
+        file_name = str(sys.argv[1])
+
+        if file_name == "":
+            raise ValueError()
+    except:
+        exit('Erreur: Le premier argument (nombre de points) doit être un entier positif.')
+
+    execute_closest_points(file_name)
