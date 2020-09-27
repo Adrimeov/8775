3796a2d5b5ebba03c0dd49b45348909a1247abe2
polarbuzzle <fersamw@gmail.com>
2020-09-27 16:20:15 -0400

    finishing data generation

--- INF8775_TP1/Data_Generation.py
+++ INF8775_TP1/Data_Generation.py
@@ -7,7 +7,6 @@ from gen import generate_samples
 NB_REPETITIONS = 100
 MAX_SAMPLES = 1000000
 SAMPLES_FILE_NAME = "samples.txt"
-DATA_FILE_NAME = "generated_data.txt"
 
 nb_samples = 100
 sample_time_pairs = []
@@ -23,10 +22,9 @@ if len(sys.argv) <= 1:
 if len(sys.argv) > 2:
     exit('Erreur: Trop d\'arguments.')
 
-# TODO: selectionner l'argument Algo
 try:
     algo_name = str(sys.argv[1])
-    if algo_name == "brute_force":
+    if algo_name == "brute":
         algo = execute_closest_points
     elif algo_name == "recursif":
         algo = execute_closest_points
@@ -42,18 +40,17 @@ while nb_samples <= MAX_SAMPLES:
     for i in range(NB_REPETITIONS):
         average_time = 0
         generate_samples(nb_samples, SAMPLES_FILE_NAME)
-
-        # TODO: call the right algo
-        # average_time += execute_closest_points(FILE_NAME, ALGO)
+        average_time += execute_closest_points(SAMPLES_FILE_NAME, algo_name)
 
     average_time /= NB_REPETITIONS
-    # print(str(nb_samples)+ " " + str(average_time))
+    print(str(nb_samples) + " " + str(average_time))
     sample_time_pairs.append((nb_samples, average_time))
 
     nb_samples *= 10  # Increase sample size at the end
 
-with open(DATA_FILE_NAME, "w") as file:
-    # TODO: get the algorithm
-    file.write("NOM DE L\"ALGO")
+data_file_name = "generated_data_" + algo_name + ".txt"
+
+with open(data_file_name, "w") as file:
+    file.write(algo_name)
     for sample_time_pair in sample_time_pairs:
         file.write(str(sample_time_pair[0]) + " " + str(sample_time_pair[1]))
