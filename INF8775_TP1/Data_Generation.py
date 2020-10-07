887d42fa6263556a85d826ff25dae59943af9056
polarbuzzle <fersamw@gmail.com>
2020-10-07 11:17:10 -0400

    writing data each iteration

--- INF8775_TP1/Data_Generation.py
+++ INF8775_TP1/Data_Generation.py
@@ -4,11 +4,12 @@ from main import execute_closest_points
 from gen import generate_samples
 
 
-NB_REPETITIONS = 5
-MAX_SAMPLES = 100000
+NB_REPETITIONS = 3
+MAX_SAMPLES = 500000
 SAMPLES_FILE_NAME = "samples.txt"
 
 nb_samples = 100
+step = 100
 sample_time_pairs = []
 
 
@@ -27,19 +28,6 @@ try:
 except:
     exit('Erreur: Le premier argument (nombre de points) doit être un entier positif.')
 
-while nb_samples <= MAX_SAMPLES:
-    average_time = 0
-
-    for i in range(NB_REPETITIONS):
-        # Generating new samples each iterations
-        generate_samples(nb_samples, SAMPLES_FILE_NAME)
-        average_time += execute_closest_points(SAMPLES_FILE_NAME, algo_name)
-
-    average_time /= NB_REPETITIONS
-    print(str(nb_samples) + " " + str(average_time))
-    sample_time_pairs.append((nb_samples, average_time))
-
-    nb_samples *= 10  # Increase sample size at the end
 
 data_file_name = "generated_data_" + algo_name + ".txt"
 
@@ -47,5 +35,21 @@ with open(data_file_name, "w") as file:
     file.write(algo_name + "\n")
     file.write(str(NB_REPETITIONS) + "\n")
 
-    for sample_time_pair in sample_time_pairs:
-        file.write(str(sample_time_pair[0]) + " " + str(sample_time_pair[1]) + "\n")
+    while nb_samples <= MAX_SAMPLES:
+
+        average_time = 0
+
+        for i in range(NB_REPETITIONS):
+            # Generating new samples each iterations
+            generate_samples(nb_samples, SAMPLES_FILE_NAME)
+            average_time += execute_closest_points(SAMPLES_FILE_NAME, algo_name)
+
+        average_time /= NB_REPETITIONS
+        print(str(nb_samples) + " " + str(average_time))
+        file.write(str(nb_samples) + " " + str(average_time) + "\n")
+
+        nb_samples += step  # Increase sample size at the end
+
+        if nb_samples / step == 10:
+            step *= 10
+
