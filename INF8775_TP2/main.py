c0ccc61cbc6fdc3fa8db4ba4a70f0ad7658cff9f
polarbuzzle <fersamw@gmail.com>
2020-10-21 15:49:24 -0400

    Ajour du main + lecture de samples

---
+++ INF8775_TP2/main.py
@@ -0,0 +1,34 @@
+import argparse
+import sys
+
+
+def read_samples(filepath):
+    blocks = []
+
+    with open(filepath, 'r') as file:
+        for line in file:
+            blocks.append(tuple(int(a) for a in line.split()))
+
+    return blocks
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) <= 1:
+        exit('Erreur: Pas assez d\'arguments. Vous devez indiquer le \
+              nombre de points à générer et le nom du fichier de sortie.')
+
+    arguments = ["--path", "--timer", "--algo", "--distance"]
+    parser = argparse.ArgumentParser()
+
+    for argument in arguments:
+        parser.add_argument(argument)
+
+    args = parser.parse_args()
+    args_as_dict = vars(args)
+    parameters = {a: vars(args)[a] for a in vars(args) if vars(args)[a] != ""}
+
+    blocks = read_samples(parameters['path'])
+    blocks.sort(key=lambda tup: (tup[1] * tup[2]) + tup[0], reverse=True)
+
+    print(blocks)
